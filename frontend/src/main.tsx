96b7af331b265c71286839d0fa3bf00c37a28437
iMADi-ARCH <nandanaditya985@gmail.com>
2023-06-25 16:20:46 +0530

    feat: use env variable for backend url

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,7 +4,7 @@ import App from "./App.tsx";
 import "./index.css";
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:5050";
+axios.defaults.baseURL = import.meta.env["VITE_BACKEND_URL"];
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
